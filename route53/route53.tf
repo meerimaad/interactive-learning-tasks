1c289e8c2b931989bf3bab22093af3ca3602354c
farrukh90 <farrukhsadykov@gmail.com>
2021-08-01 01:55:17 +0000

    added blog

--- route53/route53.tf
+++ route53/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "www" {
   zone_id = "Z32OHGRMBVZ9LR"
-  name    = "wordpress.example.com"
+  name    = "blog.example.com"
   type    = "A"
   ttl     = "300"
   records = ["127.0.0.1"]
