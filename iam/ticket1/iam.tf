bc719c1146a09033d72638f144a8d212f5778d84
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-01-30 10:12:47 -0600

    Update iam.tf

--- iam/ticket1/iam.tf
+++ iam/ticket1/iam.tf
@@ -1,13 +1,11 @@
 resource "aws_iam_user" "bob" {
   # Your code goes here
   # Do not change below tags
-  tags = local.common_tags
+  tags = local.task_tags
 }
 
 resource "aws_iam_group" "sysusers" {
   # Your code goes here
-  # Do not change below tags
-  tags = local.common_tags
 }
 
 resource "aws_iam_group_membership" "members" {
