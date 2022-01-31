4b3154827620c3ef8dbfdf895cce932c3f312174
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-01-30 22:31:32 -0600

    Update public_subnet.tf

--- vpc/ticket1/public_subnet.tf
+++ vpc/ticket1/public_subnet.tf
@@ -1,1 +1,36 @@
-# Your code goes here
+resource "aws_subnet" "public1" {
+  # Your code goes here 
+
+
+
+
+
+
+  # Please do not change below code
+  tags = local.task_tags
+}
+
+
+resource "aws_subnet" "public2" {
+   # Your code goes here 
+
+
+
+
+
+
+  # Please do not change below code
+  tags = local.task_tags
+}
+
+
+
+resource "aws_subnet" "public3" {
+   # Your code goes here 
+
+
+
+
+  # Please do not change below code
+  tags = local.task_tags
+}
