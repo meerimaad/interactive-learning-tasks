e54344eba060f262972ad18406eb54d42044c2ae
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-01-30 21:50:09 -0600

    Update and rename route53.tf to ec2.tf

---
+++ ec2/ticket1/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web" {
+  # Your code goes here 
+  
+  
+  
+  
+  
+  
+  tags = local.task_tags
+}
--- ec2/ticket1/route53.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "www" {
-  zone_id = # your code goes here
-  name    = # your code goes here
-  type    = "A"
-  ttl     = "300"
-  records = [# your code goes here]
-}
