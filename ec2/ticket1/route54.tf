97a89355ccb53f2d2fc5817ad84d180ec95ec3b0
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-01-30 21:51:38 -0600

    Create route54.tf

---
+++ ec2/ticket1/route54.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "wordpress" {
+  zone_id = # Your code goes here
+  name    = # Your code goes here
+  type    = "A"
+  ttl     = "300"
+  records = [# Your code goes here]
+}
