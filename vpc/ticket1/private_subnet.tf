96e011b17281e180bf8e09c41088a1d013ab6b6e
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-01-30 22:31:53 -0600

    Update private_subnet.tf

--- vpc/ticket1/private_subnet.tf
+++ vpc/ticket1/private_subnet.tf
@@ -1,1 +1,36 @@
-# Your code goes here
+resource "aws_subnet" "private1" {
+  # Your code goes here 
+
+
+
+
+
+
+  # Please do not change below code
+  tags = local.task_tags
+}
+
+
+resource "aws_subnet" "private2" {
+   # Your code goes here 
+
+
+
+
+
+
+  # Please do not change below code
+  tags = local.task_tags
+}
+
+
+
+resource "aws_subnet" "private3" {
+   # Your code goes here 
+
+
+
+
+  # Please do not change below code
+  tags = local.task_tags
+}
