c0f67dcf91e39e9be5b64f6be1d28027598e1695
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-01-31 21:57:23 -0600

    Update output.tf

--- rds/ticket1/output.tf
+++ rds/ticket1/output.tf
@@ -1,1 +1,16 @@
+output address {
+  # Your code goes here 
+}
+output az {
+  # Your code goes here 
+}
+output engine {
+  # Your code goes here 
+}
+output id {
+  # Your code goes here 
+}
 
+output port {
+  # Your code goes here 
+}
