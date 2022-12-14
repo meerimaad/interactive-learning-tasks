db7f7ceb26a71cb0fdf10a0325c7d94fe65307c6
meerimad <meka2188@gmail.com>
2022-12-14 01:18:50 -0600

    iam

--- iam/ticket1/iam.tf
+++ iam/ticket1/iam.tf
@@ -1,13 +1,40 @@
 resource "aws_iam_user" "bob" {
-  # Your code goes here
+  name = "bob"
   # Do not change below tags
   tags = local.task_tags
 }
-
-resource "aws_iam_group" "sysusers" {
-  # Your code goes here
+resource "aws_iam_user_login_profile" "bob" {
+  user            = aws_iam_user.bob.name
+  password_length = 46
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key,
+    ]
+  }
 }
+resource "aws_iam_group" "sysusers" {
+   name = "sysusers"
+  path = "/users/"
+}
+
 
 resource "aws_iam_group_membership" "members" {
-  # Your code goes here
+   name = "sysusers-group-membership"
+  users = [
+    aws_iam_user.user_bob.name,
+  ]
+  group = aws_iam_group.group.name
 }
+resource "aws_iam_group" "group" {
+  name = "sysusers-group"
+}
+resource "aws_iam_user" "user_bob" {
+  name = "bob-user"
+
+}
+resource "aws_iam_user" "bobs_unique_id" {
+  name = "bobs_unique_id"
+}# Your code goes here
+
