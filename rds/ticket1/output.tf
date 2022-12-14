f0c3d4c5958d881d6553634a1d32bffeca9d1cb7
meerimaad <meka2188@gmail.com>
2022-12-14 16:00:24 -0600

    output added

--- rds/ticket1/output.tf
+++ rds/ticket1/output.tf
@@ -1,16 +1,20 @@
 output address {
+  value = aws_db_instance.default.address
   # Your code goes here 
 }
 output availability_zone {
+  value = aws_db_instance.default.availability_zone
   # Your code goes here 
 }
 output engine {
+  value = aws_db_instance.default.engine
   # Your code goes here 
 }
 output id {
+  value = aws_db_instance.default.id
   # Your code goes here 
 }
-
 output port {
+  value = aws_db_instance.default.port
   # Your code goes here 
 }
