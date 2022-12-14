349cb2a31c9cc3903e98704cb1e64fd60083ca6c
meerimaad <meka2188@gmail.com>
2022-12-14 15:19:21 -0600

    ngw

--- vpc/ticket1/ngw.tf
+++ vpc/ticket1/ngw.tf
@@ -1,10 +1,10 @@
 resource "aws_eip" "example" {
-  vpc      = true
+  vpc = true
 }
 
 
-
 resource "aws_nat_gateway" "example" {
+   
   allocation_id = aws_eip.example.id
   subnet_id     = aws_subnet.public1.id
   # Please do not change below code 
@@ -12,16 +12,4 @@ resource "aws_nat_gateway" "example" {
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.gw]
   tags       = local.task_tags
-}
-
-
-
-
-
-
-  # Please do not change below code 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.gw]
-  tags = local.task_tags
 }
