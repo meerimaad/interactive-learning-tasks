d0e634ea153fce64e828683783d0c424c31e50fa
meerimaad <meka2188@gmail.com>
2022-12-14 13:00:37 -0600

    provider and route added

--- route53/ticket1/provider.tf
+++ route53/ticket1/provider.tf
@@ -1,9 +1,3 @@
 provider "aws" {
-   resource "aws_route53_record" "www" {
-      zone_id = "Z070803611T99TKR8GVFE"  
-      name    = "blog.awssimplefied.com"     
-      type    = "A"
-      ttl     = "300"
-      records = ["127.0.0.1"]
-   }
+   region = "us-east-1"
 }
--- route53/ticket1/route53.tf
+++ route53/ticket1/route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "www" {
-  zone_id =  # Your code goes here
-  name    =  # Your code goes here
+  zone_id = "Z070803611T99TKR8GVFE"  
+  name    = "blog.awssimplefied.com"     
   type    = "A"
   ttl     = "300"
-  records = [ # Your code goes here]
+  records = ["127.0.0.1"]
 }
