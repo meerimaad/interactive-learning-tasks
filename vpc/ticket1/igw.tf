9fb3f9c925667539c30279473802cf843931f50e
meerimaad <meka2188@gmail.com>
2022-12-14 15:11:19 -0600

    igw and ngw added

--- vpc/ticket1/igw.tf
+++ vpc/ticket1/igw.tf
@@ -1,5 +1,5 @@
 resource "aws_internet_gateway" "gw" {
-    # Your code goes here 
+    vpc_id = aws_vpc.main.id
 
 
     # Please do not change below code
--- vpc/ticket1/ngw.tf
+++ vpc/ticket1/ngw.tf
@@ -5,7 +5,14 @@ resource "aws_eip" "example" {
 
 
 resource "aws_nat_gateway" "example" {
-  # Your code goes here 
+  allocation_id = aws_eip.example.id
+  subnet_id     = aws_subnet.public1.id
+  # Please do not change below code 
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+  tags       = local.task_tags
+}
 
 
 
