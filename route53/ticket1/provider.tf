57be768a8e21e38f0cfb99bbba98adf1d02e8b20
meerimaad <meka2188@gmail.com>
2022-12-14 12:56:08 -0600

    provider added

--- route53/ticket1/provider.tf
+++ route53/ticket1/provider.tf
@@ -1,3 +1,9 @@
 provider "aws" {
-   # Your code goes here
+   resource "aws_route53_record" "www" {
+      zone_id = "Z070803611T99TKR8GVFE"  
+      name    = "blog.awssimplefied.com"     
+      type    = "A"
+      ttl     = "300"
+      records = ["127.0.0.1"]
+   }
 }
