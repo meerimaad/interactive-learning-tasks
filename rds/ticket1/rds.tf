d69492d2dd538e64a448328a14926bcbcfb4bf5c
meerimaad <meka2188@gmail.com>
2022-12-14 15:59:30 -0600

    rds added

--- rds/ticket1/rds.tf
+++ rds/ticket1/rds.tf
@@ -1,8 +1,15 @@
 resource "aws_db_instance" "default" {
-  # Your code goes here 
-  
-  
-  
-  # Please do not change below code
+  instance_class       = "db.t3.micro"
+  allocated_storage      = 50
+  engine               = "mysql"
+  engine_version       = "5.7"
+  username             = "foo"
+  password             = "foobarbaz" 
+  vpc_security_group_ids = [aws_security_group.db.id]
+  auto_minor_version_upgrade = true
+  max_allocated_storage = 500
+  parameter_group_name="default.mysql5.7"
+     skip_final_snapshot=true
   tags = local.task_tags
 }
+
